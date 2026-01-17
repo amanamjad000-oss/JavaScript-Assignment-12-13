1145806dce661cec8601176f7c127b2fa68f2c86
Pakeeza Noor <amanamjad000@gmail.com>
2026-01-17 20:56:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+//Chapter  12-13 (Q No 01)
+//var ch = prompt("Enter a character")
+//var input = ch.charAt(0)
+//if (input>= 48 && input <= 57)
+//{alert ("its a digit")}
+//if (input>= 97 && input <= 122)
+//{alert ("its a alphabet")}
+ //if (input>=65 && input <= 90 )   
+//{alert ("its a  upper case")}
+
+                     //***********//
+
+ //Chapter 12-13 (Q No 02)
+//var num1 = Number(prompt("Enter first integer:"));
+//var num2 = Number(prompt("Enter second integer:"));
+
+//if (num1 > num2) {
+    //document.write("Larger number is: " + num1);}
+//else if (num2 > num1) {
+   // document.write("Large number is: " + num2);}
+//else {
+   // document.write("Both numbers are equal.");}0
+
+                    //***********//
+
+//Chapter 12-13 (Q No 03)
+//var num = Number(prompt("Enter a number:"));
+
+//if (num > 0) {
+    //document.write("Number is Positive.");}
+//else if (num < 0) {
+    //document.write("Number is Negative.");}
+//else {
+    //document.write("Number is Zero.");}
+
+                     //***********//
+
+//Chapter 12-13 (Q No 04)
+//var character = prompt("Enter a alphabet:");
+
+//character = character;
+
+//if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+  //  document.write("true");}
+//else {
+   // document.write("false");}
+
+                 //************//
+
+//Cahapter 12-13 (Q No 05)
+//var correctPassword = "Pakeezanoor";
+//var userPassword = prompt("Enter your password:");
+
+//if (userPassword === "" || userPassword === null) {
+    //document.write("Please enter your password");} 
+//else if (userPassword === correctPassword) {
+   // document.write("Correct! The password you entered matches the original password");} 
+//else {
+    //document.write("Incorrect password");}
+
+              //***********//
+
+//Chapter 12-13 (Q No 06)
+//var greeting;
+//var hour = 13;
+
+//if (hour < 18) {
+  //  greeting = "Good day";}
+   // else {
+    //greeting = "Good evening";}
+//document.write(greeting);
+ 
+            //***********//
+
+//Chapter 12-13 (Q No 07)
+          //Skip//
+
+
+         //------The End--------//
